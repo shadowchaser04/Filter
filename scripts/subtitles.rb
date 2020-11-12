cef31a16849d1113eb49fd668fb5885761ff47f2
shadowchaser04 <shadowchasing@icloud.com>
2020-11-12 21:45:11 +0000

    Removed: small un-needed if statment and lots of the code dealing with tallies and anything beyond the paragraphs

--- scripts/subtitles.rb
+++ scripts/subtitles.rb
@@ -151,8 +151,8 @@ logger.info("re-created #{root_dir}") if Dir.exist?(root_dir)
 # }}}
 # {{{1 download youtube subtitles
 
-# pass in a date range to the where method returning a collection of results
-# then pluck the urls returning an array.
+# pass in a date range to the where method returning a collection, then pluck the
+# urls returning an array.
 youtube_urls = Chrome.where(:last_visit => 1.days.ago..1.days.from_now).pluck(:url)
 logger.error("DownloadYoutubeSubtitlesError: no youtube videos for the period #{1.days.ago..1.days.from_now}") if youtube_urls.count == 0
 exit if youtube_urls.count == 0
@@ -166,27 +166,32 @@ youtube_urls.each do |url|
   end
 end
 
+# }}}
+# {{{1 create the file path hash
 # create a hash with a array as values
 file_path_hash = Hash.new {|h,k| h[k] = [] }
 
 # create the filepaths.
-sub_path = sub_dir(root_dir)
+subtitle_path = sub_dir(root_dir)
 
 # loop over the file paths to organise them.
-sub_path.each do |file|
+subtitle_path.each do |file|
 
-  # create a key by getting the basename of the file and spliting of the file
-  # extentions and adding the file to the value array.
+  # create a key by removing the extention and splicing the basename.
   f = File.basename(file).split(/\./)[0]
-  (file_path_hash[f]||[]) << file if file_path_hash[f]
+
+  # add the file to the hash array.
+  (file_path_hash[f]||[]) << file
+  logger.info("Created key #{f} in the file_path_hash")
 
 end
 
+# NOTE: logger needs to tell the user what has been removed.
 # remove any videos that have not downloaded both subtitles and json file.
 file_path_hash.reject! { |k,v| v.count != 2 }
 
 # }}}
-# {{{1 build sentence array
+# {{{1 build hash containing the subtitles
 #------------------------------------------------------------------------------
 # create the hash for the subtitles
 downloaded_subs = Hash.new { |h,k| h[k] = [] }
@@ -194,60 +199,60 @@ downloaded_subs = Hash.new { |h,k| h[k] = [] }
 # loop over the file path hash.
 file_path_hash.each do |key, files|
 
-    files.each do |file|
+  # loop over the array values, two files .json and .vtt
+  files.each do |file|
 
-      # find the subtitles by there filetype.
-      if File.extname(file.split("/")[-1]) =~ /.vtt/
+    # find the subtitles by there filetype.
+    if File.extname(file.split("/")[-1]) =~ /.vtt/
+      # create an array of sentences read from the subtitles.vtt file.
+      subtitles = read_file(file)
 
-        # push the array sentences returned from the method to the hash array value.
-        subtitles = read_file(file)
+      # add the subtitles to the hash.
+      (downloaded_subs[key]||[]) << subtitles
 
-        # add the subtitles to the hash.
-        (downloaded_subs[key]||[]) << subtitles
-
-        # flatten the value array.
-        downloaded_subs.transform_values! {|value_array| value_array.flatten }
-
-      end
+      # flatten the value array.
+      downloaded_subs.transform_values! {|value_array| value_array.flatten }
+    end
 
   end
-
 end
-
 # }}}
 # {{{1 remove blacklist
 #------------------------------------------------------------------------------
-result = Hash.new {|h,k| h[k] = [] }
+result = Hash.new {|h,k| h[k] = Hash.new {|hash,key| hash[key] = []} }
 
-# Flatten out the array of arrays
-sublist = downloaded_subs['sentence'].join.split(" ")
+downloaded_subs.each do |key, value_array|
 
-# Pass in the sublist array to the Where returning an array of badwords
-# subtracted from the sublist.
-subs = (sublist - Blacklist.where(word: sublist).pluck(:word))
+  # create the array of words.
+  sublist = value_array.join.split(" ")
 
-# Group the words by themselves then count the words, sort and turn into a hash
-top_count_hash = subs.count_and_hash(10)
-# }}}
-# {{{1 create paragraphs
-#------------------------------------------------------------------------------
-top_count_hash.keys.each do |k|
-  # loops over the top ten found words, group_by groups all the nested array words
-  # and there index's. This creates a key and an array. The array contains all
-  # occurrence of the word and its index position.
-  subs = sublist.each_with_index.map {|w,i| [w,i] }.group_by {|i| i[0] }
+  # Pass in the sublist array to the Where returning an array of badwords
+  # subtracted from the sublist.
+  subs = (sublist - Blacklist.where(word: sublist).pluck(:word))
 
-  # k is queried in the hash, if found it returns an array which is flattened.
-  # it is mapped returning only the integers which are the index positions of
-  # the words.
-  subs_ints = subs["#{k}"].flatten.map {|x| Integer(x) rescue nil }.compact
+  # Group the words by themselves then count the words, sort and turn into a hash
+  top_count_hash = subs.count_and_hash(10)
 
-  # subs_ints is remapped permanently altering the array. First it creates a
-  # range of 50 words before and after i. Which is its index position. These
-  # are then joined into the paragraph.
-  subs_ints.map! {|i| pre = i - 50; pro = i + 50; pre = i if pre < 0; sublist[pre..pro].join(" ") }
-  subs_ints.each {|paragraph| (result[k]||[]) << paragraph }
+  top_count_hash.keys.each do |k|
+    # loops over the top ten found words, group_by groups all the nested array words
+    # and there index's. This creates a key and an array. The array contains all
+    # occurrence of the word and its index position.
+    subs = sublist.each_with_index.map {|w,i| [w,i] }.group_by {|i| i[0] }
+
+    # k is queried in the hash, if found it returns an array which is flattened.
+    # it is mapped returning only the integers which are the index positions of
+    # the words.
+    subs_ints = subs["#{k}"].flatten.map {|x| Integer(x) rescue nil }.compact
+
+    # subs_ints is remapped permanently altering the array. First it creates a
+    # range of 50 words before and after i. Which is its index position. These
+    # are then joined into the paragraph.
+    subs_ints.map! {|i| pre = i - 50; pro = i + 50; pre = i if pre < 0; sublist[pre..pro].join(" ") }
+    subs_ints.each {|paragraph| (result[key][k]||[]) << paragraph }
+  end
 end
+binding.pry
+
 # }}}
 # {{{1 load datasets
 #------------------------------------------------------------------------------
@@ -315,104 +320,11 @@ result.each do |k, paragraph_array|
       end
     end
     #--------------------------------------------------------------------------
-    # top words
-    #--------------------------------------------------------------------------
-    # Hash count the words of the paragraphs
-    top_ten_paragraph_words = subs.reject {|k,v| k if Blacklist.find_by(word: k) }.sort_by{|k, v| v}.reverse.first(10).to_h.symbolize_keys
-    #--------------------------------------------------------------------------
-    # count all values
-    #--------------------------------------------------------------------------
-    # Count the topics so as to rank the result based on the count.
-    count_result = Hash.new(0)
-    percent = Hash.new(0)
-    rhash.each { |k,nested_hash| count_result[:rank] += nested_hash.values.sum }
-
-    rhash.each { |k,v| percent[k] += v.values.sum }
-
-    # sum all the values to get the percentage.
-    denominator = percent.values.sum
-
-    # order the hash
-    order_topics = percent.sort_by(&:last).reverse.to_h
-
-    # devide v by the denominator which is the sum of all values and * 100 to
-    # get the percentage.
-    order_topics.transform_values! do |v|
-      n = (v / denominator.to_f*100).round(2)
-      total = n.to_s + "%"
-      total
-    end
-    #--------------------------------------------------------------------------
     # build paragraph
     #--------------------------------------------------------------------------
     # Add the paragraph and dataset hash back into the array under its key
-    (paragraph[k]||[]) << [para, top_ten_paragraph_words, rhash, count_result, order_topics]
+    (paragraph[k]||[]) << para
   end
   logger.info("Built #{k} in: #{Time.now - t}")
 end
 #}}}
-#{{{1 sum and rank
-#------------------------------------------------------------------------------
-# sort the paragraphs by there rank number. transform_values! changes the hash.
-paragraph.transform_values! { |value_array| value_array.sort_by {|paragraph_array| paragraph_array[3][:rank] } }
-#------------------------------------------------------------------------------
-# sum all rank the topics
-#------------------------------------------------------------------------------
-all_topics = Hash.new(0)
-# paragraphs_array
-# 1) paragraph
-# 2) top ten words count
-# 3) hash of database words counted
-paragraph.each do |key,arr|
-  arr.each do |topic_array|
-    topic_array[2].each { |k,v| all_topics[k] += v.values.sum }
-  end
-end
-#}}}
-#{{{1 display output
-#------------------------------------------------------------------------------
-# symbolz are quicker, have a uniq id.
-paragraph_symbolz = paragraph.deep_symbolize_keys
-paragraph_symbolz.each do |k,v|
-  puts "#{blue(k)})\n"
-  v.each_with_index do |par,i|
-    puts "#{i})"
-    par.each {|item| puts item }
-    puts "\n"
-  end
-end
-
-#{{{1 json
-#------------------------------------------------------------------------------
-# there are two files in the sub_path. *.json, *.vtt
-# select the json returning an array.
-file = sub_path.select {|f| f if File.extname(f.split("/")[-1]) =~ /.json/ }
-
-if file.class == Array && file[0] =~ /.json/
-  # open and parse json file
-  data = JSON.parse(File.read(file[0]))
-else
-  log_to_logfile.error("RetriveJsonDataError: program closed due to #{file} error. no .json file.")
-  logger.error("Error: no j.son data - check log for more thorough explanation")
-  exit
-end
-#}}}
-#{{{1 percentage of all words
-# sum all the values to get the percentage.
-denominator = all_topics.values.sum
-
-order_topics = all_topics.sort_by(&:last).reverse.to_h
-
-order_topics.each do |k,v|
-  n = (v / denominator.to_f*100).round(2)
-  total = n.to_s + "%"
-  puts "#{k} ->  #{total}"
-end
-#}}}
-#{{{1 build db
-#------------------------------------------------------------------------------
-yt_user = User.find_or_create_by(uploader: data['uploader'], channel_id: data['channel_id'])
-re = yt_user.youtube_results.find_or_create_by(title: data['title'])
-re.update(duration: data['duration'], meta_data: {total: all_topics, top_count: top_count_hash})
-#}}}
-
